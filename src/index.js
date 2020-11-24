f414b67eb0fd97fb3c86a85aa7be76c62f9da0b2
SilentSierra <51127618+SilentSierra@users.noreply.github.com>
2020-11-24 16:34:17 +0000

    Attempt at randGhost

--- src/index.js
+++ src/index.js
@@ -13,6 +13,28 @@ const usernameContainer = document.querySelector('#username');
 var prob = 0.5 // (50%) percent probability of getting "true"
 var randomBoolean = Boolean(Math.random() <= prob);
 
+//this section will have the ghosts be a random colour from the avaiable 4 options
+//not fully working however; for a given client all ghosts are came colour, not all different
+//also, a ghost player will have a phantom ghost at their spawn point of their colour,
+//while they will be Blinky (red) and not the correct random colour
+
+// let rand = Math.floor((Math.random() * 3) + 1);
+// let randGhost;
+// if (rand == 0){
+//     randGhost = [OBJECT_TYPE.BLINKY];
+// }
+// else if (rand == 1){
+//     randGhost = [OBJECT_TYPE.INKY];
+// }
+// else if (rand == 2){
+//     randGhost = [OBJECT_TYPE.PINKY];
+// }
+// else if (rand == 3){
+//     randGhost = [OBJECT_TYPE.CLYDE];
+// }
+let randGhost = [OBJECT_TYPE.BLINKY];
+
+
 // socket.io connection setup
 const socket = io(`ws://${window.location.host}`);
 const connectedPromise = new Promise(resolve => {
@@ -60,6 +82,7 @@ for (var i = 0; i < LAYOUT.length; i++) {
 const maxFood = Math.floor(emptyCells.length / 4);
 let currentFood = 0;
 
+
 function checkCollision(pacman, ghosts) {
 
 }
@@ -98,13 +121,13 @@ function gameLoop(player) {
         }
 
         //check if pacman eats other players
-        if (player.powerPill && arena.objectExist(player.pos, OBJECT_TYPE.BLINKY)) {
-            arena.removeObject(player.pos, [OBJECT_TYPE.BLINKY]);
+        if (player.powerPill && arena.objectExist(player.pos, randGhost)) {
+            arena.removeObject(player.pos, randGhost);
             score += 100;
         }
 
         // check if pacman is eaten by a ghost
-        if (arena.objectExist(player.pos, OBJECT_TYPE.BLINKY)) {
+        if (arena.objectExist(player.pos, randGhost)) {
             gameOver();
         }
     }
@@ -130,8 +153,9 @@ function gameLoop(player) {
     socket.on('position', ({pos, bool, rot}) => {
 
         // Decide whether to spawn pacman or ghost
+
         if (bool) playerType = [OBJECT_TYPE.PACMAN];
-        if (!bool) playerType = [OBJECT_TYPE.BLINKY];
+        if (!bool) playerType = randGhost;
 
         // My very janky way of getting rid of duplicate pacmen
         // It simply checks the 4 cells around the passed in pacman, if the pacman object exists there
@@ -151,12 +175,12 @@ function gameLoop(player) {
 
         arena.addObject(pos, playerType);
 
-        if (!arena.objectExist(pos, [OBJECT_TYPE.BLINKY])) arena.rotateDiv(pos, rot);
+        if (!arena.objectExist(pos, randGhost)) arena.rotateDiv(pos, rot);
     })
 
     socket.on('removal', pos => {
         arena.removeObject(pos, [OBJECT_TYPE.PACMAN])
-        arena.removeObject(pos, [OBJECT_TYPE.BLINKY])
+        arena.removeObject(pos, randGhost)
         gameOver();
     })
 
@@ -192,7 +216,7 @@ function startGame(){
         arena.addObject(randomPos, [OBJECT_TYPE.PACMAN]);
     } else {
         player = new Ghost(2, randomPos);
-        arena.addObject(randomPos, [OBJECT_TYPE.BLINKY]);
+            arena.addObject(randomPos, randGhost);
     }
 
     //
